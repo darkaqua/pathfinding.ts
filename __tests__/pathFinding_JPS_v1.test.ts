79dc49bd84b4ac12258ca5963b84941dba838493
pablo <pagoru@gmail.com>
2020-12-31 17:52:53 +0100

    Fixed build + v1.3.2

--- __tests__/pathFinding_JPS_v1.test.ts
+++ __tests__/pathFinding_JPS_v1.test.ts
@@ -1,9 +1,96 @@
 import {FinderEnum, Grid} from '../src';
 
-// @ts-ignore
-import {smallGrid, testCasesSmallGrid} from "./test-data/small-grid";
-// @ts-ignore
-import {testCasesBigGrid} from "./test-data/big-grid";
+export const smallGrid = [
+    [50, 55, 60, 65, 70],
+    [45, 0,  0,  0,  75],
+    [40, 0,  0,  0,  1 ],
+    [35, 0,  0,  0,  5 ],
+    [30, 25, 20, 15, 10],
+];
+
+export const testCasesSmallGrid = [
+    {
+        grid: smallGrid,
+        startPoint: { x: 4, y: 2 },
+        endPoint: { x: 4, y: 1 },
+        maxJumpCost: 5,
+        path: [
+            { x: 4, y: 2 },
+            { x: 4, y: 4 },
+            { x: 0, y: 4 },
+            { x: 0, y: 0 },
+            { x: 4, y: 0 },
+            { x: 4, y: 1 },
+        ],
+    },
+    {
+        grid: smallGrid,
+        startPoint: { x: 4, y: 2 },
+        endPoint: { x: 4, y: 1 },
+        maxJumpCost: 10,
+        path: [
+            { x: 4, y: 2 },
+            { x: 4, y: 4 },
+            { x: 0, y: 4 },
+            { x: 0, y: 0 },
+            { x: 4, y: 0 },
+            { x: 4, y: 1 },
+        ],
+    },
+    {
+        grid: smallGrid,
+        startPoint: { x: 4, y: 2 },
+        endPoint: { x: 4, y: 1 },
+        maxJumpCost: 100,
+        path: [
+            { x: 4, y: 2 },
+            { x: 4, y: 1 },
+        ],
+    },
+    {
+        grid: smallGrid,
+        startPoint: { x: 4, y: 2 },
+        endPoint: { x: 4, y: 1 },
+        maxJumpCost: 1,
+        path: [],
+    },
+    {
+        grid: smallGrid,
+        startPoint: { x: 2, y: 0 },
+        endPoint: { x: 4, y: 4 },
+        maxJumpCost: 10,
+        path: [
+            { x: 2, y: 0 },
+            { x: 0, y: 0 },
+            { x: 0, y: 4 },
+            { x: 4, y: 4 },
+        ],
+    },
+    {
+        grid: smallGrid,
+        startPoint: { x: 4, y: 3 },
+        endPoint: { x: 0, y: 1 },
+        maxJumpCost: 5,
+        path: [
+            { x: 4, y: 3 },
+            { x: 4, y: 4 },
+            { x: 0, y: 4 },
+            { x: 0, y: 1 },
+        ],
+    },
+    {
+        grid: smallGrid,
+        startPoint: { x: 0, y: 1 },
+        endPoint: { x: 4, y: 3 },
+        maxJumpCost: 5,
+        path: [
+            { x: 0, y: 1 },
+            { x: 0, y: 4 },
+            { x: 4, y: 4 },
+            { x: 4, y: 3 },
+        ],
+    }
+]
 
 describe('test JPS v1', () => {
     const testCases = [
