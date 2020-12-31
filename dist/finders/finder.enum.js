ec410638ed09e259e36c39ad2ea75f6db76deb1a
pablo <pagoru@gmail.com>
2020-12-31 17:38:46 +0100

    Deleted dist + v1.3.1

--- dist/finders/finder.enum.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FinderEnum = void 0;
-var FinderEnum;
-(function (FinderEnum) {
-    FinderEnum[FinderEnum["JUMP_POINT"] = 0] = "JUMP_POINT";
-})(FinderEnum = exports.FinderEnum || (exports.FinderEnum = {}));
--- dist/objects/point/point.d.ts
+++
@@ -1,8 +0,0 @@
-import { PointInterface } from "./point.interface";
-export declare class Point implements PointInterface {
-    x: number;
-    y: number;
-    constructor(x: number, y: number);
-    copy(x: number, y: number): Point;
-    equal(point: Point): boolean;
-}
